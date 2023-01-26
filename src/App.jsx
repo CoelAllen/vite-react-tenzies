bfd8b6dd253c75d4302e1858ca2ce4d918456b68
Coel Allen <coelallen@gmail.com>
2023-01-25 18:40:37 -0700

    border/description--placeholder for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="border--box">
+        <div className="inner--box">
+          <div className="text--box">
+            <h1>Tenzies</h1>
+            <p className="description">
+              Roll until all dice are the same. Click each die to freeze it at
+              its current value between rolls.
+            </p>
+          </div>
+          <div>Dice go here</div>
+          <div>Roll button go here</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
