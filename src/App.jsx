cc3510424fa342785ad656335048a9b31d4cff7e
Coel Allen <coelallen@gmail.com>
2023-01-26 11:38:56 -0700

    rolls  and held working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Die from "./Die.jsx";
 
 function App() {
+  const [allDice, setAllDice] = useState(allNewDice());
+  const [rolls, setRolls] = useState(0);
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        id: i,
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+      });
+    }
+    return newDice;
+  }
+  function rollDice() {
+    setRolls((prev) => prev + 1);
+    setAllDice((prev) =>
+      prev.map((d) => {
+        return d.isHeld === false
+          ? { ...d, value: Math.ceil(Math.random() * 6) }
+          : d;
+      })
+    );
+  }
+  function holdDie(id) {
+    setAllDice((prev) =>
+      prev.map((d) => {
+        return d.id === id ? { ...d, isHeld: !d.isHeld } : d;
+      })
+    );
+  }
+
+  const dice = allDice.map((d) => (
+    <Die
+      key={d.id}
+      value={d.value}
+      isHeld={d.isHeld}
+      holdDie={() => holdDie(d.id)}
+    />
+  ));
   return (
     <main>
       <div className="border--box">
@@ -14,19 +54,13 @@ function App() {
               its current value between rolls.
             </p>
           </div>
-          <div className="dice--box">
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
-            <Die />
+          <div className="dice--box">{dice}</div>
+          <div>
+            <div className="roll--button" onClick={rollDice}>
+              Roll
+            </div>
+            <div className="rolls">Rolls: {rolls}</div>
           </div>
-          <div>Roll button go here</div>
         </div>
       </div>
     </main>
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function Die() {
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59e391" : "white",
+  };
   return (
     <div className="die--main">
-      <div className="die--box">
-        <div className="die--number">1</div>
+      <div className="die--box" style={styles} onClick={props.holdDie}>
+        <div className="die--number">{props.value}</div>
       </div>
     </div>
   );
